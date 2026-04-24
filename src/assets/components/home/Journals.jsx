a08df1fa933b21c4251aa68b0f437c251c618bb1
gopi2401 <gopicoc2401@gmail.com>
2026-04-24 11:46:04 +0530

    journal to 2025

--- src/assets/components/home/Journals.jsx
+++ src/assets/components/home/Journals.jsx
@@ -40,20 +40,21 @@ export default function ConferenceCarousel() {
     },
     {
       type: "Research article • Restricted access",
+      title:
+        "Mechanism of PI3K-AKT Signaling Pathway Mediating Apoptosis of Periodontal Tissue Cells in Chronic Periodontitis",
+      authors: "Lan Dazhi, Pimporn Thongmuang, Sarisak Soontornchai and You Jianpeng",
+      publication: "International Journal of Environmental Sciences",
+      year: "2025",
+      pages: "",
+    },
+    {
+      type: "Research article • Restricted access",
       title: "Redefining educational success: Analyzing the impact of closed management systems on student well-being and academic performance in Chinese technical education.",
       authors: "Jingkun Liu, Rovena I Dellova and Zhengxin Yi",
       publication: "E-Learning and Digital Media",
       year: "2024",
     },
-    {
-      type: "Research article • Restricted access",
-      title:
-        "Mechanism of PI3K-AKT Signaling Pathway Mediating Apoptosis of Periodontal Tissue Cells in Chronic Periodontitis",
-      authors: "Lan Dazhi, Pimporn Thongmuang, Sarisak Soontornchai and You Jianpeng",
-      publication: "International Journal of Environmental Sciences",
-      year: "2024",
-      pages: "",
-    },
+    
     {
       type: "Research article • Restricted access",
       title: "Zeolite X from coal fly ash inhibits proliferation of human breast cancer cell lines (MCF-7) via induction of S phase arrest and apoptosis",
